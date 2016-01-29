2132e75c415760910ee361d6cb066109aac0efe7
Insane-96 <insane96mcp@gmail.com>
2016-01-29 23:18:31 +0100

    Removed the f**kin' .suo

--- Enviroment/Enviroment.cs
+++ Enviroment/Enviroment.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Aiv.Engine;
 
 namespace GlobalGameJam2016.Enviroment
 {
-	class Enviroment
+	class Enviroment:GameObject
 	{
+
+		public Enviroment()
+		{
+			
+		}
+
+		public override void Start()
+		{
+			base.Start();
+		}
+
+		public override void Update()
+		{
+			base.Update();
+		}
 	}
 }
