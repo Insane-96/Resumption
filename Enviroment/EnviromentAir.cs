2b7955344e651cb6d6c479890952f665ae10adf7
TheShinobi94 <robertofolco94@hotmail.com>
2016-01-30 17:29:05 +0100

    Add Enviroment air

---
+++ Enviroment/EnviromentAir.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalGameJam2016.Enviroment
+{
+    class EnviromentAir : Enviroment
+    {
+        public EnviromentAir(int width, int height) : base(width, height)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+    }
+}
